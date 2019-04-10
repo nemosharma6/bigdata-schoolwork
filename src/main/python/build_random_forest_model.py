abcc419e4634669087a871a94120773f94dc953d
Nimish Sharma <sharmanemo1994@gmail.com>
2019-04-10 12:20:30 -0500

    random forest classifier

---
+++ src/main/python/build_random_forest_model.py
@@ -0,0 +1,99 @@
+from kafka import KafkaConsumer
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, CountVectorizer
+from pyspark.ml.classification import RandomForestClassifier
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from nltk.corpus import stopwords
+import requests
+
+
+def get_data(u):
+    json_data = requests.get(u).json()
+    d = []
+
+    for ind in range(len(json_data["response"]['results'])):
+        headline = json_data["response"]['results'][ind]['fields']['headline']
+        body_text = json_data["response"]['results'][ind]['fields']['bodyText']
+        headline += ". "
+        headline += body_text
+        label = json_data["response"]['results'][ind]['sectionName']
+        temp = list()
+        temp.append(label)
+        temp.append(headline)
+        d.append(temp)
+
+    return d
+
+
+if __name__ == "__main__":
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+
+    consumer = KafkaConsumer(
+        'guardian11',
+        bootstrap_servers=['localhost:9092'],
+        auto_offset_reset='earliest',
+        enable_auto_commit=True,
+        group_id='test1',
+        api_version=(0, 10),
+        consumer_timeout_ms=1000,
+        value_deserializer=lambda x: x.decode('utf-8'))
+
+    data = []
+    for i in consumer:
+        tmp = list()
+        tmp.append(i.value.split("||")[0])
+        tmp.append(i.value.split("||")[1])
+        data.append(tmp)
+
+    print(len(data))
+    df = sqlContext.createDataFrame(data, schema=["category", "text"])
+
+    # regular expression tokenizer
+    regex_tokenizer = RegexTokenizer(inputCol="text", outputCol="words", pattern="\\W")
+
+    # stop words
+    stop_words = list(set(stopwords.words('english')))
+
+    stop_words_remover = StopWordsRemover(inputCol="words", outputCol="filtered").setStopWords(stop_words)
+
+    # bag of words count
+    count_vectors = CountVectorizer(inputCol="filtered", outputCol="features", vocabSize=10000, minDF=5)
+    label_string_index = StringIndexer(inputCol="category", outputCol="label")
+    label_string_index.setHandleInvalid("keep")
+
+    pipeline = Pipeline(stages=[regex_tokenizer, stop_words_remover, count_vectors, label_string_index])
+    (training_data, test_data) = df.randomSplit([0.8, 0.2], seed=100)
+    pipeline_fit = pipeline.fit(training_data)
+    pipeline_fit.save("pipeline2")
+
+    training_data_set = pipeline_fit.transform(training_data)
+    training_data_set.show(5)
+
+    # stages = pipeline_fit.stages
+    # vec = [s for s in stages if isinstance(s, CountVectorizerModel)]
+    # v1 = vec[0].vocabulary
+    # print(len(v1))
+
+    print("Training: " + str(training_data_set.count()))
+    print("Test: " + str(test_data.count()))
+
+    lr = RandomForestClassifier(labelCol="label", featuresCol="features", numTrees=100, maxDepth=4, maxBins=32)
+    lr_model = lr.fit(training_data_set)
+
+    test_data_set = pipeline_fit.transform(test_data)
+    predictions = lr_model.transform(test_data_set)
+    test_data_set.show(20)
+    # predictions.filter(predictions['prediction'] == 0) \
+    #     .select("text", "category", "probability", "label", "prediction") \
+    #     .orderBy("probability", ascending=False) \
+    #     .show(n=10, truncate=30)
+
+    evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+    percent = evaluator.evaluate(predictions)
+    print(percent * 100)
+
+    lr_model.save("random_forest_model.model")
---
+++ src/main/python/run_random_forest.py
@@ -0,0 +1,55 @@
+import requests
+from pyspark.sql import SQLContext
+from pyspark import SparkContext
+from pyspark.ml import PipelineModel
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import RandomForestClassificationModel
+# import sys
+
+
+def get_data(u):
+    json_data = requests.get(u).json()
+    d = []
+
+    for ind in range(len(json_data["response"]['results'])):
+        headline = json_data["response"]['results'][ind]['fields']['headline']
+        body_text = json_data["response"]['results'][ind]['fields']['bodyText']
+        headline += ". "
+        headline += body_text
+        label = json_data["response"]['results'][ind]['sectionName']
+        temp = list()
+        temp.append(label)
+        temp.append(headline)
+        d.append(temp)
+
+    return d
+
+
+if __name__ == "__main__":
+
+    sc = SparkContext()
+    sqlContext = SQLContext(sc)
+
+    random_forest_model = RandomForestClassificationModel.load("random_forest_model.model")
+
+    if len(sys.argv) != 4:
+        print('Number of arguments is not correct')
+        exit()
+    
+    key = sys.argv[1]
+    from_date = sys.argv[2]
+    to_date = sys.argv[3]
+
+    url = 'http://content.guardianapis.com/search?from-date=' + from_date + '&to-date=' + to_date + \
+          '&order-by=newest&show-fields=all&page-size=200&%20num_per_section=10000&api-key=' + key
+
+    data = get_data(url)
+    df = sqlContext.createDataFrame(data, schema=["category", "text"])
+    pipeline_fit = PipelineModel.load("pipeline2")
+    data_set = pipeline_fit.transform(df)
+
+    predictions = random_forest_model.transform(data_set)
+
+    evaluator = MulticlassClassificationEvaluator(predictionCol="prediction")
+    percent = evaluator.evaluate(predictions)
+    print(percent * 100)
